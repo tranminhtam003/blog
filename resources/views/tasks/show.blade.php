436531079aa803e6d8cad5dcafd346dd6797d847
tranminhtam003 <tmtam@tma.com.vn>
2017-12-15 16:03:23 +0700

    + seconde commit

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,10 +9,6 @@
     <script src="main.js"></script>
 </head>
 <body>
-    <ul>
-        @foreach ($tasks as $task)
-            <li>{{ $task->body }}</li>
-        @endforeach
-    </ul>
+    <h1>{{ $task->body }}</h1>
 </body>
 </html>
